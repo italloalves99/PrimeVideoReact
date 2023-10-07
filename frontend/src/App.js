f997e4db5190c7cbcbedcf7751130dd17e4fde1b
italloalves99 <134318969+italloalves99@users.noreply.github.com>
2023-10-06 21:15:52 -0300

    cronometro

---
+++ frontend/src/App.js
@@ -0,0 +1,73 @@
+import React, { useState, useEffect } from 'react';
+
+
+
+export default function App() {
+
+  
+
+  const [hora,setHora] = useState(4);
+
+  const [minuto, setMinuto] = useState(59);
+
+  const [segundo,setSegundo] = useState(45);
+
+
+
+  
+
+  useEffect(() => {
+
+    
+
+    
+
+      const interval = setInterval(() => {
+
+          
+
+          setSegundo(segundo+1);
+
+          if(segundo == 59){
+
+            setSegundo(0);
+
+            setMinuto(minuto+1);
+
+            if(minuto == 59){
+
+              setMinuto(0);
+
+              setHora(hora+1);
+
+            }
+
+          }
+
+      }, 1000);
+
+
+
+      return () => clearInterval(interval);
+
+    
+
+    
+
+  });
+
+ 
+
+  return (
+
+    <div>
+
+      <h2 style={{textAlign:'center',color:'red'}}>{hora}:{minuto}:{segundo}</h2>
+
+    </div>
+
+  );
+
+
+
+}
